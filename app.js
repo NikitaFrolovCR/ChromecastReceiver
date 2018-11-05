fdec5463aef8b304099d075a54d65b05dcf58307
alexander_sulyaev_cr <alexander.sulyaev.cr@gmail.com>
2018-11-05 16:04:10 +0200

    ### features - cors installed

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use('/assets', express.static('assets'));
 
 app.get('/', function (req, res) {
